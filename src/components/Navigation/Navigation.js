b8635d9cf167d17aa58f322b49ac932970d292a7
Kimi82 <cyrekamil@gmail.com>
2021-05-29 00:56:58 +0200

    changing nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,13 +2,13 @@ import React, {useState, useEffect, useRef} from 'react';
 import './Navigation.css'
 import logo from '../static/logo.png'
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+//import { TweenLite } from "gsap";
 
 
 
 function Navigation() {
 
-  
 
 
 
@@ -22,7 +22,7 @@ const [activeDot, setActiveDot] = useState("about");
   <h2>YOURNAME</h2></a> */}
 
 <ul className="navigation__list">
-<li onClick={()=>setActiveDot("about")}><a href="#about" ><span>O Nas</span><div className="dot"/></a></li>
+<li onClick={()=>setActiveDot("about")}><a href="#about"><span>O Nas</span><div className="dot"/></a></li>
 <li onClick={()=>setActiveDot("services")}><a href="#services"><span>Usługi</span><div className="dot"/></a></li>
 <li onClick={()=>setActiveDot("realizations")}><a href="#realizations"><span>Realizacje</span><div className="dot-active"/></a></li>
 <li onClick={()=>setActiveDot("contact")}><a href="#contact"><span>Kontakt</span><div className="dot"/></a></li>
