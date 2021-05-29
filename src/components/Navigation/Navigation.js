95aecd89bc35b779bbf5eb86cb1e152613bc1974
Kimi82 <cyrekamil@gmail.com>
2021-05-29 14:39:58 +0200

    still nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,23 +10,36 @@ import {ScrollTrigger} from 'gsap/ScrollTrigger'
 function Navigation() {
 
 
-
-
-
-const [activeDot, setActiveDot] = useState("about");
-
   return (
-<section className="navigation__wrapper">
+<section className="navigation">
 {/* <a href="/" className="navigation__logo">
 <img src={logo}/>
   <h2>YOURNAME</h2></a> */}
 
-<ul className="navigation__list">
-<li onClick={()=>setActiveDot("about")}><a href="#about"><span>O Nas</span><div className="dot"/></a></li>
-<li onClick={()=>setActiveDot("services")}><a href="#services"><span>Usługi</span><div className="dot"/></a></li>
-<li onClick={()=>setActiveDot("realizations")}><a href="#realizations"><span>Realizacje</span><div className="dot-active"/></a></li>
-<li onClick={()=>setActiveDot("contact")}><a href="#contact"><span>Kontakt</span><div className="dot"/></a></li>
-</ul>
+
+<a href="#about">
+  <div className="navigation__tooltip">
+    <div className="navigation__dot">
+      <span className="navigation__tooltipText">About</span>
+  </div></div></a>
+
+<a href="#services">
+  <div className="navigation__tooltip">
+    <div className="navigation__dot">
+      <span className="navigation__tooltipText">Services</span>
+  </div></div></a>
+
+<a href="#realizations">
+  <div className="navigation__tooltip">
+    <div className="navigation__dot">
+      <span className="navigation__tooltipText">Realizations</span>
+  </div></div></a>
+
+<a href="#contact">
+  <div className="navigation__tooltip">
+    <div className="navigation__dot">
+      <span className="navigation__tooltipText">About</span>
+    </div></div></a>
 </section> 
     
   );
