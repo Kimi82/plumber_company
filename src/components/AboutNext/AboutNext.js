3d1a9ca861e911b23d13daa26826b26726e0396e
Kimi82 <cyrekamil@gmail.com>
2021-06-16 23:59:19 +0200

    scrollTrigger START dosnt work

--- src/components/AboutNext/AboutNext.js
+++ src/components/AboutNext/AboutNext.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './AboutNext.css'
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+
 gsap.registerPlugin(ScrollTrigger);
 
-const wrapper = document.querySelector(".aboutNext__textWrapper");
-const paragraphs = document.querySelectorAll('p')
-console.log(wrapper)
 
-gsap.fromTo(paragraphs, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: .5, duration: 2, scrollTrigger: {
-    trigger: wrapper,
-    start: 'top 20%',
-    markers: true
-}})
+
 
 function AboutNext() {
+
+
+
+  
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  useEffect(() => {
+    const wrapper = document.querySelector(".aboutNext__textWrapper");
+    const paragraphs = document.querySelectorAll('p');
+  async function demo() {
+   // await sleep(2000);
+    console.log(paragraphs)
+    gsap.fromTo(paragraphs, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: .5, duration: 2, scrollTrigger: {
+      trigger: wrapper,
+      start: 'top 20%',
+      markers: true,
+    }})
+
+  }
+  demo();
+   }, [])
 
     return (
     <section className="aboutNext slide" id="aboutNext">
